05bdb09e15e7ff6f3e91713b0bd25131baa9275d
Tony Zhang <tzhang07@arizona.edu>
2023-10-25 10:56:49 -0700

    Changed Node<E> -> Node<T>

--- AVLTree/src/Node.java
+++ AVLTree/src/Node.java
@@ -1,37 +1,37 @@
-class Node<E> {
-	private E value;
-	private Node<E> right;
-	private Node<E> left;
-	private Node<E> parent;
+class Node<T> {
+	private T value;
+	private Node<T> right;
+	private Node<T> left;
+	private Node<T> parent;
 	
-	public Node(E item){
+	public Node(T item){
 		this.value = item;
 		this.right = null;
 		this.left = null;
 		this.parent = null;
 	}
 	
-	public Node getRight() {
+	public Node<T> getRight() {
 		return right;
 	}
 	
-	public Node getLeft() {
+	public Node<T> getLeft() {
 		return left;
 	}
 	
-	public Node getParent() {
+	public Node<T> getParent() {
 		return parent;
 	}
 	
-	public void setLeft(Node node) {
+	public void setLeft(Node<T> node) {
 		this.left = node;
 	}
 	
-	public void setRight(Node node) {
+	public void setRight(Node<T> node) {
 		this.right = node;
 	}
 	
-	public void setParent(Node node) {
+	public void setParent(Node<T> node) {
 		this.parent = node;
 	}
 	
