7234f97b47f7daa58cef06a76dcd06263c3faa7c
JezielBG-UofA <126594724+JezielBG-UofA@users.noreply.github.com>
2023-11-06 14:58:12 -0700

    print tree function

--- AVLTree/src/AVLTree.java
+++ AVLTree/src/AVLTree.java
@@ -1,164 +1,171 @@
+import java.util.LinkedList;
 import java.util.Queue;
 
-public class AVLTree<T extends Comparable<T>>  {
+public class AVLTree<T extends Comparable<T>> {
 	private Node<T> root;
 	private int size;
+
 	public AVLTree() {
 		this.root = null;
 		size = 0;
 	}
-	
+
 	public AVLTree(Node<T> root) {
 		this.root = root;
 		size = 1;
 	}
-	
+
 	public void insert(T value) {
 		root = insertHelper(root, value);
 	}
-	
-	private Node<T> insertHelper(Node<T> root, T value){
-		if(root == null) {
+
+	private Node<T> insertHelper(Node<T> root, T value) {
+		if (root == null) {
 			root = new Node<T>(value);
 			size++;
 			return root;
 		}
 		int compareValue = compare(value, root.value);
-		
-		if(compareValue <= 0) {
+
+		if (compareValue <= 0) {
 			root.left = insertHelper(root.left, value);
+		} else if (compareValue > 0) {
+			root.right = insertHelper(root.right, value);
 		}
-		else if(compareValue > 0) {
-			 root.right = insertHelper(root.right, value);
-		}
-		
+
 		// TODO:
-		// 		Balancing/Rotating and updating the weight
-		
+		// Balancing/Rotating and updating the weight
+
 		return root;
 	}
-	
+
 	// searching for a specific value
 	public T search(T value) {
 		return searchHelper(root, value);
 	}
-	
+
 	// returns null if not found
 	private T searchHelper(Node<T> root, T value) {
 		Node<T> tempRoot = root;
-		
-		if(this.root == null) {
+
+		if (this.root == null) {
 			return null;
 		}
-		
-		while(tempRoot != null) {
-			if(compare(value, tempRoot.value) == 0) {
+
+		while (tempRoot != null) {
+			if (compare(value, tempRoot.value) == 0) {
 				return tempRoot.value;
-			}
-			else if(compare(value, tempRoot.value) < 0) {
+			} else if (compare(value, tempRoot.value) < 0) {
 				tempRoot = tempRoot.left;
-			}
-			else {
+			} else {
 				tempRoot = tempRoot.right;
 			}
 		}
-		
+
 		return null;
 	}
-	
-	
-	public T getMin(){
+
+	public T getMin() {
 		Node<T> tempNode = root;
-		if(tempNode == null) {
+		if (tempNode == null) {
 			return null;
 		}
-		
-		while(tempNode.left != null) {
+
+		while (tempNode.left != null) {
 			tempNode = tempNode.left;
 		}
 		return tempNode.value;
 	}
-	
+
 	public T getMax() {
 		Node<T> tempNode = root;
-		if(tempNode == null) {
+		if (tempNode == null) {
 			return null;
 		}
-		
-		while(tempNode.right != null) {
+
+		while (tempNode.right != null) {
 			tempNode = tempNode.right;
 		}
 		return tempNode.value;
 	}
-	
-	
-	
+
 	public void deleteValue(T value) {
 		deleteHelper(root, value);
 	}
-	
+
 	private Node<T> deleteHelper(Node<T> root, T value) {
-		if(root == null) {
+		if (root == null) {
 			size = size - 1;
 			return null;
 		}
-		if(compare(value, root.value) < 0) {
+		if (compare(value, root.value) < 0) {
 			root.left = deleteHelper(root.left, value);
-		}
-		else if(compare(value, root.value) > 0) {
+		} else if (compare(value, root.value) > 0) {
 			root.right = deleteHelper(root.right, value);
-		}
-		else {
-			if(root.left == null) {
+		} else {
+			if (root.left == null) {
 				return root.right;
-			}
-			else if(root.right == null) {
+			} else if (root.right == null) {
 				return root.left;
 			}
-			
+
 			root.value = minOfSubtree(root.right);
 			root.right = deleteHelper(root.right, root.value);
 		}
-		
-		
+
 		// TODO:
-		// 		Balancing/Rotating and updating the weight
-		
+		// Balancing/Rotating and updating the weight
+
 		return root;
 	}
-	
-	
+
 	private T minOfSubtree(Node<T> root) {
 		T minValue = root.value;
-        while (root.left != null) {
-            minValue = root.left.value;
-            root = root.left;
-        }
-        return minValue;
+		while (root.left != null) {
+			minValue = root.left.value;
+			root = root.left;
+		}
+		return minValue;
 	}
-	
-	
+
 	private int compare(T insertValue, T nodeValue) {
 		return insertValue.compareTo(nodeValue);
 	}
-	
+
 	public int getSize() {
 		return size;
 	}
-	
+
 	public T getLeft() {
 		return root.left.value;
 	}
-	
+
 	public T getRight() {
 		return root.right.value;
 	}
-	
-	
-	
+
 	public void printTree() {
-		
+		Queue<String> queue = new LinkedList<>();
+		Queue<Node<T>> treeTraversalQueue = new LinkedList<>();
+		treeTraversalQueue.add(root);
+
+		while (!treeTraversalQueue.isEmpty()) {
+			Node<T> curNode = treeTraversalQueue.remove();
+			queue.add(curNode.toString());
+			if (curNode.left != null) {
+				treeTraversalQueue.add(curNode.left);
+			}
+			if (curNode.right != null) {
+				treeTraversalQueue.add(curNode.right);
+			}
+		}
+
+		while(!queue.isEmpty()) {
+			System.out.print(queue.remove());
+		}
+
 	}
+
 	
-	
+
 }
