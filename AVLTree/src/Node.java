9b8b549686ec54a1b2e25a01381a5c0707ab54c6
tzhang07 <tzhang07@arizona.edu>
2023-11-07 20:26:06 -0700

    node updates

--- AVLTree/src/Node.java
+++ AVLTree/src/Node.java
@@ -4,12 +4,17 @@ class Node<T> {
 	public Node<T> left;
 	public Node<T> parent;
 	public int weight;
+	public int height;
+	public int quantity;
 	
 	public Node(T item){
 		this.value = item;
 		this.right = null;
 		this.left = null;
 		this.parent = null;
+		this.height = 0;
+		this.quantity = 1;
 	}
+
 	
 }
