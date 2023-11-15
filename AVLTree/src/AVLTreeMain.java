b08c8d1ddcf96f2d5fa0257f670cc4562772d56d
tzhang07 <tzhang07@arizona.edu>
2023-11-15 11:08:49 -0700

    changed main testing.

--- AVLTree/src/AVLTreeMain.java
+++ AVLTree/src/AVLTreeMain.java
@@ -1,8 +1,12 @@
 
 public class AVLTreeMain {
 	public static void main(String[] args) {
+		buildSmallTree1();
+	}
+	
+	private static void testDelete() {
 		AVLTree<Integer> newAvlTree = new AVLTree<Integer>();
-		System.out.println("\nInside test Delete");
+//		System.out.println("\nInside test Delete");
 		newAvlTree.insert(14);
 		newAvlTree.insert(5);
 		newAvlTree.insert(36);
@@ -16,4 +20,17 @@ public class AVLTreeMain {
 		newAvlTree.deleteValue(15);
 		newAvlTree.deleteValue(21);
 	}
+	
+	private static void buildSmallTree1() {
+		AVLTree<Integer> newAvlTree = new AVLTree<Integer>();
+		newAvlTree.insert(5);
+		newAvlTree.insert(2);
+		newAvlTree.insert(6);
+		newAvlTree.printTree(); // 5 with 2 children: L: 2 R: 6
+		System.out.println(); // should print 5 2 6
+		newAvlTree.insert(7);
+		newAvlTree.insert(8);
+		newAvlTree.printTree(); // should print 5 2 7 6 8
+	}
 }
+
