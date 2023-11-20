f1be1016dd64b37d6758b15278a50f8c265807ec
tzhang07 <tzhang07@arizona.edu>
2023-11-20 16:00:55 -0700

    added more test cases

--- AVLTree/src/AVLTreeMain.java
+++ AVLTree/src/AVLTreeMain.java
@@ -21,6 +21,9 @@ public class AVLTreeMain {
 			buildSmallTree1();
 			buildBigTree2();
 			testDelete();
+			testDelete2();
+			testDelete3();
+			testDelete4();
 		}
 		else if(makeTree.compareTo(s.toLowerCase()) == 0) {
 			String quit = "quit";
@@ -113,16 +116,144 @@ public class AVLTreeMain {
 		System.out.println("Before deleting:");
 		newAvlTree.printTree();
 		newAvlTree.deleteValue(14);
-		System.out.println("\nAfter deleting:");
+		System.out.println("\n\nAfter deleting 14:");
+		System.out.println("Expected:\n15 5 36 2 10 21 55 28\nActual:");
 		newAvlTree.printTree();
 		newAvlTree.deleteValue(15);
-		System.out.println("\nAfter deleting 15:");
+		System.out.println("\n\nAfter deleting 15:");
+		System.out.println("Expected:\n21 5 36 2 10 28 55\nActual:");
 		newAvlTree.printTree();
 		newAvlTree.deleteValue(21);
-		System.out.println("\nAfter deleting: 21");
+		System.out.println("\n\nAfter deleting: 21");
+		System.out.println("Expected:\n28 5 36 2 10 55\nActual:");
+		newAvlTree.printTree();
+		newAvlTree.deleteValue(36);
+		System.out.println("\n\nAfter deleting 36:");
+		System.out.println("Expected:\n28 5 55 2 10\nActual:");
 		newAvlTree.printTree();
 		System.out.println("\n===============================================");
 
+	}
+	
+	private static void testDelete2() {
+		AVLTree<Integer> newAvlTree = new AVLTree<Integer>();
+		System.out.println("===============================================");
+		newAvlTree.insert(9);
+		newAvlTree.insert(8);
+		newAvlTree.insert(15);
+		newAvlTree.insert(7);
+		newAvlTree.insert(13);
+		newAvlTree.insert(20);
+		newAvlTree.insert(10);
+		System.out.println("TESTING DELETE2 WITH TREE:");
+		newAvlTree.printTree();
+		System.out.println("\nDelete 8:\nExpected:\n13 9 15 7 10 20\nActual:");
+		newAvlTree.deleteValue(8);
+		newAvlTree.printTree();
+		
+		
+		
+		System.out.println("\n===============================================");
+	}
+	
+	
+	private static void testDelete3() {
+		AVLTree<Integer> newAvlTree = new AVLTree<Integer>();
+		System.out.println("\n===============================================");
+		newAvlTree.insert(50);
+		newAvlTree.insert(25);
+		newAvlTree.insert(75);
+		newAvlTree.insert(15);
+		newAvlTree.insert(60);
+		newAvlTree.insert(35);
+		newAvlTree.insert(120);
+		newAvlTree.insert(10);
+		newAvlTree.insert(68);
+		newAvlTree.insert(90);
+		newAvlTree.insert(125);
+		newAvlTree.insert(83);
+		newAvlTree.insert(100);
+		System.out.println("\nTESTING DELETE3 WITH TREE:");
+		newAvlTree.printTree();
+		System.out.println("\n\nDeleting 120");
+		newAvlTree.deleteValue(120);
+		System.out.println("Expected:\n50 25 75 15 35 60 90 10 68 83 125 100\nActual:");
+		newAvlTree.printTree();
+		System.out.println("\n\nDeleting 10");
+		System.out.println("Expected:\n75 50 90 25 60 83 125 15 35 68 100\nActual:");
+		newAvlTree.deleteValue(10);
+		newAvlTree.printTree();
+		System.out.println("\n===============================================");
+	}
+	
+	private static void testDelete4() {
+		AVLTree<Integer> avlTree = new AVLTree<>();
+
+        // Test Case 1: Insertion
+        avlTree.insert(10);
+        avlTree.insert(5);
+        avlTree.insert(15);
+        avlTree.insert(3);
+        avlTree.insert(7);
+        avlTree.insert(12);
+        avlTree.insert(17);
+        System.out.println("AVL Tree after insertion:");
+        avlTree.printTree();
+        System.out.println();
+
+        /*
+         * Expected AVL Tree:
+         *      10
+         *     /  \
+         *    5    15
+         *   / \   / \
+         *  3   7 12  17
+         */
+
+        // Test Case 2: Deletion
+        avlTree.deleteValue(5);
+        System.out.println("AVL Tree after deleting 5:");
+        avlTree.printTree();
+        System.out.println();
+
+        /*
+         * Expected AVL Tree:
+         *      10
+         *     /  \
+         *    7    15
+         *   /     / \
+         *  3     12  17
+         */
+
+        // Test Case 3: Deletion causing rotations
+        avlTree.deleteValue(15);
+        System.out.println("AVL Tree after deleting 15:");
+        avlTree.printTree();
+        System.out.println();
+
+        /*
+         * Expected AVL Tree:
+         *     10
+         *     / \
+         *    7   17
+         *   /    /
+         *  3    12  
+         */
+        
+        // Test Case 4: Deletion causing double rotations
+        avlTree.deleteValue(10);
+        System.out.println("AVL Tree after deleting 10:");
+        avlTree.printTree();
+        System.out.println();
+        /*
+         * Expected AVL Tree:
+         *     12         
+         *     / \      
+         *    7  17 
+         *   /     
+         *  3      
+         */
+		System.out.println("\n===============================================");
 	}
 	
 	private static void test2() {
@@ -184,8 +315,8 @@ public class AVLTreeMain {
 	private static void buildBigTree2() {
 		AVLTree<Integer> newAvlTree = new AVLTree<Integer>();
 		System.out.println("===============================================");
-		System.out.println("TESTING BIG TREE WITH 50 NODES");
-		for(int i = 1; i <= 26; i++) {
+		System.out.println("TESTING BIG TREE WITH 26 NODES");
+		for(int i = 0; i < 26; i++) {
 			newAvlTree.insert(i);
 			if(i > 0 && i % 2 == 0) {
 				System.out.println("\nPrinting Tree after inserting: " + i + " nodes:");
@@ -197,5 +328,6 @@ public class AVLTreeMain {
 		newAvlTree.printTree();
 		System.out.println("\n===============================================");
 	}
+	
 }
 
