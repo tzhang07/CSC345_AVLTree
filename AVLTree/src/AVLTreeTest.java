8e043632e152f8f883b722d61c9ff60e31569241
NMette <nmette03@gmail.com>
2023-11-21 20:04:26 -0700

    AVLTreeTest Comments

--- AVLTree/src/AVLTreeTest.java
+++ AVLTree/src/AVLTreeTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.Test;
 
 class AVLTreeTest {
 	
-	//private HashSet<Integer> numbers = new HashSet<>();
 	
+	/**
+	 * Creates a random AVLTree of size ~100 and confirms the height of each subtree
+	 * to the left and right of the root is roughly equal.
+	 */
 	@Test
 	void heightTest() {
 		Random rand = new Random();
@@ -28,6 +31,10 @@ class AVLTreeTest {
 		assertTrue(currTree.getSize() == numbers.size());
 	}
 	
+	/**
+	 * Generates a random AVLTree of size ~50 and attempts to search for each value within the tree
+	 * to confirm that it can find each value after being built.
+	 */
 	@Test
 	void searchTest() {
 		Random rand = new Random();
@@ -46,6 +53,10 @@ class AVLTreeTest {
 		}
 	}
 	
+	/**
+	 * Tests to see if the the delete method in the AVLTree is able to delete each value after being built
+	 * without compromising the total organization and balance of the tree.
+	 */
 	@Test
 	void deleteTest() {
 		Random rand = new Random();
@@ -75,6 +86,10 @@ class AVLTreeTest {
 		}
 	}
 	
+	/**
+	 * Creates an AVLTree of size 100, containing the values 0-99.
+	 * Checks to see if the values of the min and the max are stored correctly.
+	 */
 	@Test
 	void checkMinAndMax() {
 		HashSet<Integer> numbers = new HashSet<>();
@@ -90,6 +105,10 @@ class AVLTreeTest {
 		assertTrue(currTree.getSize() == numbers.size());
 	}
 	
+	/**
+	 * Creates and AVLTree with and ordered list and assures that the balance of the tree is kept
+	 * and that the size is correct.
+	 */
 	@Test
 	void checkOrderedList() {
 		AVLTree<Integer> currTree = new AVLTree<>();
@@ -103,6 +122,10 @@ class AVLTreeTest {
 		assertTrue(currTree.getSize() == 100);
 	}
 	
+	/**
+	 * Creates an empty tree and assures that the methods getMax, getMin, and getSize return the
+	 * corresponding values for an empty tree.
+	 */
 	@Test
 	void checkEmptyTree() {
 		AVLTree<Integer> currTree = new AVLTree<>();
